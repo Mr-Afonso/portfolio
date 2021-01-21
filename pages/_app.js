6c13a2c12837f371d606ef0d04fa66c16d24308d
Miguel Afonso <Miguel@g2g.local>
2021-01-21 15:30:09 +0100

    SSR debugging

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,3 @@
-import App from 'next/app';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '@/styles/index.scss'
 
@@ -7,22 +6,27 @@ import Hero from '@/components/shared/Hero'
 
 
 const MyApp = ({ Component, pageProps }) => {
+
+  const isHomePage = () => Component.name === 'Home'
+
   return (
     <div className="portfolio-app">
       <NavBar />
-      {pageProps.appData}
-      {Component.name === 'Home' && <Hero />}
+      {isHomePage() && <Hero />}
       <div className="container">
         <Component {...pageProps} />
       </div>
+      {
+        isHomePage() &&
+        <footer id="sticky-footer" className="py-4 bg-black text-white-50 py-3">
+          <div className="container text-center">
+            <small>Copyright &copy; Your Website</small>
+          </div>
+        </footer>
+      }
+
     </div>
   )
-}
-
-MyApp.getInitialProps = async (context) => {
-const initialProps = App.getInitialProps && await App.getInitialProps(context)
-
-  return { pageProps: { appData: 'Hello _App Component', ...initialProps.pageProps } }
 }
 
 export default MyApp
