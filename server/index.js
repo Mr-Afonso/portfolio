09df0effeb1e30db1abbbe35102b8433f92e87e4
Miguel Afonso <Miguel@g2g.local>
2021-01-24 12:00:54 +0100

    Portfolio resolvers

--- server/index.js
+++ server/index.js
@@ -8,20 +8,65 @@ const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
+const data = {
+  portfolios: [
+    {
+      _id: 'csc3234vsd',
+      title: 'Job in USA',
+      content: 'It was a very nice experience',
+      jobTitle: 'Chef',
+      daysOfExperience: 100,
+      isCurrentEmployed: false
+    },
+    {
+      _id: 'zxczxc35',
+      title: 'Job in Portugal',
+      content: 'It was a very good experience',
+      jobTitle: 'Developer',
+      isCurrentEmployed: true
+    },
+    {
+      _id: 'dfgdf566',
+      title: 'Job in UK',
+      content: 'It was a very solid experience',
+      jobTitle: 'Manager',
+      daysOfExperience: 30,
+      isCurrentEmployed: false
+    },
+  ]
+}
+
 app.prepare().then(() => {
   const server = express()
 
   // construct a schema using GraphQL shema language
   const schema = buildSchema(`
-  type Query {
-    hello: String
-  }
+    type Portfolio {
+      _id: ID!
+      title: String
+      content: String
+      jobTitle: String
+      daysOfExperience: Int
+      isCurrentEmployed: Boolean
+    }
+
+    type Query {
+      hello: String
+      portfolio: Portfolio
+      portfolios: [Portfolio]
+    }
   `)
 
   // the root provide a resolver for each API endpoint
   const root = {
     hello: () => {
       return 'Hello World!'
+    },
+    portfolio: () => {
+      return data.portfolios[0]
+    },
+    portfolios: () => {
+      return data.portfolios
     }
   }
 
