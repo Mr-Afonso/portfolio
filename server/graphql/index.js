dc431dd611908c50cfe1f44fed4b3477b6070790
Miguel Afonso <Miguel@g2g.local>
2021-01-27 16:44:55 +0100

    User infrastructure

--- server/graphql/index.js
+++ server/graphql/index.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose')
 const { ApolloServer, gql } = require('apollo-server-express')
 
-const { portfolioQueries, portfolioMutations } = require('./resolvers')
+const { portfolioQueries, portfolioMutations, userMutations } = require('./resolvers')
 const { portfolioTypes } = require('./types')
 const Portfolio = require('./models/Portfolio')
+const User = require('./models/User')
 
 exports.createApolloServer = () => {
   // construct a schema using GraphQL shema language
@@ -19,6 +20,10 @@ exports.createApolloServer = () => {
       createPortfolio(input: PortfolioInput): Portfolio
       updatePortfolio(id: ID, input: PortfolioInput): Portfolio
       deletePortfolio(id: ID): ID
+
+      signIn: String
+      signUp: String
+      signOut: String
     }
   `
 
@@ -28,7 +33,8 @@ exports.createApolloServer = () => {
       ...portfolioQueries
     },
     Mutation: {
-      ...portfolioMutations
+      ...portfolioMutations,
+      ...userMutations
     }
   }
 
@@ -37,7 +43,8 @@ exports.createApolloServer = () => {
     resolvers,
     context: () => ({
       models: {
-        Portfolio: new Portfolio(mongoose.model('Portfolio'))
+        Portfolio: new Portfolio(mongoose.model('Portfolio')),
+        User: new User()
       }
     })
   })
--- server/graphql/resolvers/index.js
+++ server/graphql/resolvers/index.js
@@ -1,5 +1,3 @@
-const Portfolio = require('../../database/models/portfolio')
-
 exports.portfolioQueries = {
   portfolio: (root, { id }, ctx) => {
     return ctx.models.Portfolio.getById(id)
@@ -11,7 +9,7 @@ exports.portfolioQueries = {
 
 exports.portfolioMutations = {
   createPortfolio: async (root, { input }, ctx) => {
-    const createdPortfolio = await Portfolio.create(input)
+    const createdPortfolio = await ctx.models.Portfolio.create(input)
     return createdPortfolio
   },
   updatePortfolio: async (root, { id, input }, ctx) => {
@@ -22,4 +20,17 @@ exports.portfolioMutations = {
     const deletedPortfolio = await ctx.models.Portfolio.findAndDelete(id)
     return deletedPortfolio._id
   }
+}
+
+exports.userMutations = {
+  signIn: (root, args, ctx) => {
+    return ctx.models.User.signIn()
+  },
+  signUp: (root, args, ctx) => {
+    return ctx.models.User.signUp()
+  },
+  signOut: (root, args, ctx) => {
+    return ctx.models.User.signOut()
+  }
+
 }
