3c1cee0b4e0cb589184b48853ff111ce290171f3
Miguel Afonso <Miguel@g2g.local>
2021-01-27 21:53:34 +0100

    Find user in DB

--- server/graphql/context/index.js
+++ server/graphql/context/index.js
@@ -4,25 +4,22 @@ const passport = require('passport');
 // options == {email, password}
 const authenticateUser = (options) => {
   return new Promise((resolve, reject) => {
-    console.log("Calling authenticateUser");
+    const done = (error, user) => {
 
-    const done = (err, user) => {
-      // Here we will get user if user is authenticated
-      // If we will get user we can save session to DB
-
-      if (err) {
-        return reject(new Error(err));
+      if (error) {
+        return reject(new Error(error));
       }
 
+      // If we will get user we can save session to DB
       if (user) {
         return resolve(user);
+      } else {
+        return reject(new Error('Invalid password or email!'));
       }
     }
 
     const authFn = passport.authenticate('graphql', options, done);
     authFn();
-
-  return true;
   })
 }
 
