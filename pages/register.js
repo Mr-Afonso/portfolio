36961b5ce6ceda680e302b71e17d7a48abf2f91b
Miguel Afonso <Miguel@g2g.local>
2021-01-28 11:36:46 +0100

    Redirect

--- pages/register.js
+++ pages/register.js
@@ -2,6 +2,7 @@ import RegisterForm from '@/components/forms/RegisterForm';
 import { Mutation } from 'react-apollo';
 import { SIGN_UP } from '@/apollo/queries';
 import withApollo from '@/hoc/withApollo';
+import Redirect from '@/components/shared/Redirect';
 
 const Register = () => {
 
@@ -17,6 +18,7 @@ const Register = () => {
                   <RegisterForm onSubmit={registerData => {
                     signUpUser({ variables: registerData })
                   }} />
+                  { data && data.signUp && <Redirect to="/login" /> }
                 </>
               }
             </Mutation>
