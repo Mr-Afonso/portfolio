92a23bbc17a38b6259358f90b8eaf9f5dda95724
Miguel Afonso <Miguel@g2g.local>
2021-02-09 16:50:41 +0100

    Create topic functionality

--- apollo/actions/index.js
+++ apollo/actions/index.js
@@ -1,5 +1,5 @@
 import { useQuery, useMutation, useLazyQuery } from '@apollo/react-hooks';
-import { GET_PORTFOLIOS, GET_PORTFOLIO, GET_USER_PORTFOLIOS, CREATE_PORTFOLIO, UPDATE_PORTFOLIO, DELETE_PORTFOLIO, SIGN_IN, SIGN_OUT, GET_USER, FORUM_CATEGORIES,  TOPICS_BY_CATEGORY } from '@/apollo/queries'
+import { GET_PORTFOLIOS, GET_PORTFOLIO, GET_USER_PORTFOLIOS, CREATE_PORTFOLIO, UPDATE_PORTFOLIO, DELETE_PORTFOLIO, SIGN_IN, SIGN_OUT, GET_USER, FORUM_CATEGORIES, TOPICS_BY_CATEGORY, CREATE_TOPIC } from '@/apollo/queries'
 
 export const useGetPortfolios = () => useQuery(GET_PORTFOLIOS);
 export const useGetPortfolio = (options) => useQuery(GET_PORTFOLIO, options);
@@ -7,7 +7,7 @@ export const useGetUserPortfolios = () => useQuery(GET_USER_PORTFOLIOS);
 export const useUpdatePortfolio = () => useMutation(UPDATE_PORTFOLIO)
 export const useDeletePortfolio = () => useMutation(DELETE_PORTFOLIO, {
   update(cache, { data: { deletePortfolio } }) {
-    const {userPortfolios} = cache.readQuery({query: GET_USER_PORTFOLIOS})
+    const { userPortfolios } = cache.readQuery({ query: GET_USER_PORTFOLIOS })
     const newPortfolios = userPortfolios.filter(p => p._id !== deletePortfolio);
     cache.writeQuery({
       query: GET_USER_PORTFOLIOS,
@@ -51,4 +51,24 @@ export const useGetUser = () => useQuery(GET_USER)
 export const useGetForumCategories = () => useQuery(FORUM_CATEGORIES)
 
 export const useGetTopicsByCategory = (options) => useQuery(TOPICS_BY_CATEGORY, options)
+
+export const useCreateTopic = () => useMutation(CREATE_TOPIC, {
+  update(cache, { data: { createTopic } }) {
+    try {
+      const { topicsByCategory } = cache.readQuery({
+        query: TOPICS_BY_CATEGORY, variables: {
+          category: createTopic.forumCategory.slug
+        }
+      });
+      cache.writeQuery({
+        query: TOPICS_BY_CATEGORY,
+        data: { topicsByCategory: [...topicsByCategory, createTopic] },
+        variables: {
+          category: createTopic.forumCategory.slug
+        }
+      });
+    } catch (e) { }
+  }
+})
+
 // Forum actions End -----------------------
