e70e553d8e77f3b6b64623b0843c267d398f3564
Miguel Afonso <Miguel@g2g.local>
2021-02-11 13:47:57 +0100

    Prepare posts data

--- server/fakeDb/FakeDb.js
+++ server/fakeDb/FakeDb.js
@@ -1,9 +1,10 @@
-const { portfolios, users, forumCategories, topics } = require('./data');
+const { portfolios, users, forumCategories, topics, posts } = require('./data');
 
 const Portfolio = require('../database/models/portfolio')
 const User = require('../database/models/user');
 const ForumCategory = require('../database/models/forumCategory');
 const Topic = require('../database/models/topic');
+const Post = require('../database/models/post');
 
 class FakeDb {
 
@@ -12,6 +13,7 @@ class FakeDb {
     await Portfolio.deleteMany({})
     await ForumCategory.deleteMany({});
     await Topic.deleteMany({});
+    await Post.deleteMany({});
   }
 
   async addData() {
@@ -19,6 +21,7 @@ class FakeDb {
     await Portfolio.create(portfolios)
     await ForumCategory.create(forumCategories);
     await Topic.create(topics);
+    await Post.deleteMany({});
   }
 
   async populate() {
