d2d5e56a577d9f30e07bd0f4e061f0dab03f5140
Miguel Afonso <Miguel@g2g.local>
2021-02-13 13:38:08 +0100

    Display posts

--- pages/forum/topics/[slug].js
+++ pages/forum/topics/[slug].js
@@ -3,21 +3,21 @@ import { useGetTopicBySlug, useGetPostsByTopic } from '@/apollo/actions'
 import { useRouter } from 'next/router';
 import withApollo from '@/hoc/withApollo';
 import { getDataFromTree } from '@apollo/react-ssr';
-
+import PostItem from '@/components/forum/PostItem';
 
 const useInitialData = () => {
   const router = useRouter();
   const { slug } = router.query;
-  const { data: dataT } = useGetTopicBySlug({variables: { slug }});
-  const { data: dataP } = useGetPostsByTopic({variables: { slug }});
+  const { data: dataT } = useGetTopicBySlug({ variables: { slug } });
+  const { data: dataP } = useGetPostsByTopic({ variables: { slug } });
   const topic = (dataT && dataT.topicBySlug) || {};
   const posts = (dataP && dataP.postsByTopic) || [];
   return { topic, posts };
 }
 
 const Posts = () => {
-  const { topic } = useInitialData();
-  
+  const { topic, posts  } = useInitialData();
+
   return (
     <BaseLayout>
       <section className="section-title">
@@ -29,127 +29,24 @@ const Posts = () => {
       </section>
       <section>
         <div className="fj-post-list">
-          <div className="row">
-            <div className="col-md-9">
-              <div className="topic-post">
-                <article>
-                  <div className="row">
-                    <div className="topic-avatar">
-                      <div className="main-avatar">
-                        <img
-                          className="avatar subtle-shadow"
-                          src="https://i.imgur.com/cVDadwb.png"></img>
-                      </div>
-                    </div>
-                    <div className="topic-body">
-                      <div className="topic-header">
-                        <div className="topic-meta">
-                          <div className="name-container">
-                            <span className="name">Filip Jerga</span>
-                          </div>
-                          <div className="date-container"><span className="date">21h</span></div>
-                        </div>
-                      </div>
-                      <div className="topic-content">
-                        <div className="cooked">
-                          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-                        </div>
-                        <section className="post-menu-area">
-                          <nav className="post-controls">
-                            <div className="actions">
-                              <button className="btn">reply</button>
-                            </div>
-                          </nav>
-                        </section>
-                      </div>
-                    </div>
-                  </div>
-                </article>
+
+          {topic._id &&
+            <PostItem
+              post={topic}
+              className="topic-post-lead" />
+          }
+          {posts.map(post =>
+            <div key={post._id} className="row">
+              <div className="col-md-9">
+                <PostItem post={post} />
               </div>
             </div>
-          </div>
-          <div className="row">
-            <div className="col-md-9">
-              <div className="topic-post">
-                <article>
-                  <div className="row">
-                    <div className="topic-avatar">
-                      <div className="main-avatar">
-                        <img
-                          className="avatar subtle-shadow"
-                          src="https://i.imgur.com/cVDadwb.png"></img>
-                      </div>
-                    </div>
-                    <div className="topic-body">
-                      <div className="topic-header">
-                        <div className="topic-meta">
-                          <div className="name-container">
-                            <span className="name">Filip Jerga</span>
-                          </div>
-                          <div className="date-container"><span className="date">21h</span></div>
-                        </div>
-                      </div>
-                      <div className="topic-content">
-                        <div className="cooked">
-                          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-                        </div>
-                        <section className="post-menu-area">
-                          <nav className="post-controls">
-                            <div className="actions">
-                              <button className="btn">reply</button>
-                            </div>
-                          </nav>
-                        </section>
-                      </div>
-                    </div>
-                  </div>
-                </article>
-              </div>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-md-9">
-              <div className="topic-post">
-                <article>
-                  <div className="row">
-                    <div className="topic-avatar">
-                      <div className="main-avatar">
-                        <img
-                          className="avatar subtle-shadow"
-                          src="https://i.imgur.com/cVDadwb.png"></img>
-                      </div>
-                    </div>
-                    <div className="topic-body">
-                      <div className="topic-header">
-                        <div className="topic-meta">
-                          <div className="name-container">
-                            <span className="name">Filip Jerga</span>
-                          </div>
-                          <div className="date-container"><span className="date">21h</span></div>
-                        </div>
-                      </div>
-                      <div className="topic-content">
-                        <div className="cooked">
-                          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-                        </div>
-                        <section className="post-menu-area">
-                          <nav className="post-controls">
-                            <div className="actions">
-                              <button className="btn">reply</button>
-                            </div>
-                          </nav>
-                        </section>
-                      </div>
-                    </div>
-                  </div>
-                </article>
-              </div>
-            </div>
-          </div>
+          )
+          }
         </div>
       </section>
     </BaseLayout>
   )
 }
 
-export default withApollo(Posts, {getDataFromTree});
+export default withApollo(Posts, { getDataFromTree });
--- server/fakeDb/data.js
+++ server/fakeDb/data.js
@@ -26,7 +26,7 @@ const data = {
   users: [
     {
       _id: user1Id,
-      avatar: "https://f0.pngfuel.com/png/312/283/man-face-clip-art-png-clip-art-thumbnail.png",
+      avatar: "https://cdn1.iconfinder.com/data/icons/ninja-things-1/1772/ninja-simple-512.png",
       email: "filip99@gmail.com",
       name: "Filip Jerga",
       username: "Filip99",
@@ -36,8 +36,7 @@ const data = {
     },
     {
       _id: user2Id,
-      avatar: "https://f0.pngfuel.com/png/312/283/man-face-clip-art-png-clip-art-thumbnail.png",
-      email: "test@gmail.com",
+      avatar: "https://cdn.icon-icons.com/icons2/1736/PNG/512/4043260-avatar-male-man-portrait_113269.png",
       name: "Test User",
       username: "Test99",
       info: "Hello I am Test and I am a test!",
